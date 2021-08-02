01ec28220c4b8756237616d82ec45b0a4f903834
anfaz <78058791+anfaz@users.noreply.github.com>
2021-08-02 00:08:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Sliding feature for login/register home
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
@@ -9,3 +10,43 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+//calendar functions
+var app = {
+	settings: {
+	  container: $('.calendar'),
+	  calendar: $('.front'),
+	  days: $('.weeks span'),
+	  form: $('.back'),
+	  input: $('.back input'),
+	  buttons: $('.back button')
+	},
+  
+	init: function() {
+	  instance = this;
+	  settings = this.settings;
+	  this.bindUIActions();
+	},
+  
+	swap: function(currentSide, desiredSide) {
+	  settings.container.toggleClass('flip');
+  
+	  currentSide.fadeOut(900);
+	  currentSide.hide();
+	  desiredSide.show();
+  
+	},
+  
+	bindUIActions: function() {
+	  settings.days.on('click', function(){
+		instance.swap(settings.calendar, settings.form);
+		settings.input.focus();
+	  });
+  
+	  settings.buttons.on('click', function(){
+		instance.swap(settings.form, settings.calendar);
+	  });
+	}
+  }
+  
+  app.init();
